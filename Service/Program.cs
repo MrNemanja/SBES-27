323e91ab31b6a522e02d63284fa6e59a44e16a39
Sibirjak13 <119792795+Sibirjak13@users.noreply.github.com>
2023-01-13 20:15:33 +0100

    Sertifikati

--- Service/Program.cs
+++ Service/Program.cs
@@ -8,6 +8,9 @@ using Contract;
 using SecurityManager;
 using System.ServiceModel.Description;
 using System.IdentityModel.Policy;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Security;
+using System.Security.Principal;
 
 namespace Service
 {
@@ -15,20 +18,33 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            /// srvCertCN.SubjectName should be set to the service's username. .NET WindowsIdentity class provides information about Windows user running the given process
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+
+
             NetTcpBinding binding = new NetTcpBinding();
             string address = "net.tcp://localhost:9999/IDatabaseManagement";
 
             binding.Security.Mode = SecurityMode.Transport;
             binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
             binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
-
+            
             ServiceHost host = new ServiceHost(typeof(ImplDatabaseManag));
             host.AddServiceEndpoint(typeof(IDatabaseManagement), binding, address);
 
-        
+            ///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+
+            ///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople, StoreLocation.LocalMachine, "wcfservice");
+
+
             host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
             host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
-
 
             // podesavamo custom polisu, odnosno nas objekat principala
             host.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
